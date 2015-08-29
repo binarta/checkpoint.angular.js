c8765ecb47d5095ecef5856771f9572ec3f0aa34
Jo Kerkhofs <jke@thinkerit.be>
2015-08-29 10:42:28 +0200

    Added checkpoint shop templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
     minifyHtml = require('gulp-minify-html'),
+    template = require('gulp-template'),
     templateCache = require('gulp-angular-templatecache');
+
+var minifyHtmlOpts = {
+    empty: true,
+    cdata: true,
+    conditionals: true,
+    spare: true,
+    quotes: true
+};
 
 gulp.task('templates-bootstrap3', function () {
     gulp.src('template/bootstrap3/**/bin-*.html')
@@ -9,4 +18,12 @@ gulp.task('templates-bootstrap3', function () {
         .pipe(gulp.dest('src/main/js'));
 });
 
-gulp.task('default', ['templates-bootstrap3']);
+gulp.task('templates-shop-bootstrap3', function () {
+    gulp.src('template/bootstrap3/*.html')
+        .pipe(template({shop: true}))
+        .pipe(minifyHtml(minifyHtmlOpts))
+        .pipe(templateCache('checkpoint-shop-tpls-bootstrap3.js', {standalone: true, module: 'checkpoint.templates'}))
+        .pipe(gulp.dest('src/main/js'));
+});
+
+gulp.task('default', ['templates-bootstrap3', 'templates-shop-bootstrap3']);
