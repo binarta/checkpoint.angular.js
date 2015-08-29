685ed035d8dfd5fe8123d479cce0262547aae7f0
Jo Kerkhofs <jke@thinkerit.be>
2015-08-29 16:05:09 +0200

    fixed templates

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,15 @@ var minifyHtmlOpts = {
 };
 
 gulp.task('templates-bootstrap3', function () {
-    gulp.src('template/bootstrap3/**/bin-*.html')
-        .pipe(minifyHtml())
+    gulp.src('template/bootstrap3/bin-*.html')
+        .pipe(template({shop: false}))
+        .pipe(minifyHtml(minifyHtmlOpts))
         .pipe(templateCache('checkpoint-tpls-bootstrap3.js', {standalone: true, module: 'checkpoint.templates'}))
         .pipe(gulp.dest('src/main/js'));
 });
 
 gulp.task('templates-shop-bootstrap3', function () {
-    gulp.src('template/bootstrap3/*.html')
+    gulp.src('template/bootstrap3/bin-*.html')
         .pipe(template({shop: true}))
         .pipe(minifyHtml(minifyHtmlOpts))
         .pipe(templateCache('checkpoint-shop-tpls-bootstrap3.js', {standalone: true, module: 'checkpoint.templates'}))
