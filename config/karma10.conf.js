1d5b77b0ba104c4d5fb57d65377bc3d347ef03ea
wro <wouter@websters.be>
2018-03-10 14:30:32 +0100

    configured karma for ChromeHeadless by default and added missing sources which came from update transitive dependency

--- config/karma10.conf.js
+++ config/karma10.conf.js
@@ -6,9 +6,11 @@ module.exports = function(config) {
             {pattern:'bower_components/angular/angular.js'},
             {pattern:'bower_components/angular-route/angular-route.js'},
             {pattern:'bower_components/angular-mocks/angular-mocks.js'},
+            {pattern:'bower_components/moment/moment.js'},
             {pattern:'bower_components/binartajs/src/binarta.js'},
             {pattern:'bower_components/binartajs/src/checkpoint.js'},
             {pattern:'bower_components/binartajs/src/gateways.inmem.js'},
+            {pattern:'bower_components/binarta.web.storage/src/web.storage.js'},
             {pattern:'bower_components/binartajs-angular1/src/binarta-angular.js'},
             {pattern:'bower_components/binartajs-angular1/src/binarta-checkpoint-angular.js'},
             {pattern:'bower_components/binartajs-angular1/src/binarta-checkpoint-inmem-angular.js'},
@@ -16,9 +18,8 @@ module.exports = function(config) {
             {pattern:'bower_components/thk-rest-client-mock/src/rest.client.mock.js'},
             {pattern:'bower_components/thk-notifications-mock/src/notifications.mock.js'},
             {pattern:'bower_components/binarta.web.storage.angular/src/web.storage.js'},
-            {pattern:'src/**/*.js'},
-            {pattern:'test/**/*.js'}
+            {pattern:'src/**/*.js'}
         ],
-        browsers:['PhantomJS']
+        browsers:['ChromeHeadless']
     });
 };
